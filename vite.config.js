4d6d306ccecb3f9e23076ce42c97233bd474d5c1
Muhammad Suffyan Abbasi <Ahmed@capiplan.com>
2025-10-15 12:36:08 +0500

    inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), 
   ],
-   base: "/abbas-app/", 
-   server:{
-    open:true,
-    port:9001,
-   },
+  base: "/abbas-app"
 })
